c79731daddb656a58ebc1f33817f87a8fc8fa40f
Matt <matt.cooperr@gmail.com>
2019-08-22 10:29:13 +0200

    new update

--- server.js
+++ server.js
@@ -102,12 +102,12 @@ const options = {
   useFindAndModify: false
 }
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-
-  //   app.get("*", (req, res) =>
-  //   res.sendFile(path.resolve("build", "index.html"))
-  // )
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 // DATABASE CONNECTION
